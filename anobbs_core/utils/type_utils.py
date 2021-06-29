6903dfdcc6a0e6dc4ddc85ea7e0dda6013928a50
Thoxvi <Thoxvi@Gmail.com>
2021-06-29 17:30:04 +0800

    fix type utils

--- anobbs_core/utils/type_utils.py
+++ anobbs_core/utils/type_utils.py
@@ -10,5 +10,5 @@ class EnumType:
             getattr(cls, attr)
             for attr
             in dir(cls)
-            if not attr.startswith("__")
+            if not attr.startswith("__") and attr != "get_list"
         ]
