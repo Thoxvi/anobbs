94e8239c7fc1774c6a197cbe3a932bee0bd96d0a
Thoxvi <Thoxvi@Gmail.com>
2021-07-01 17:02:54 +0800

    change id length

--- anobbs_core/utils/id_utils.py
+++ anobbs_core/utils/id_utils.py
@@ -2,10 +2,8 @@ __all__ = [
     "get_uuid",
 ]
 
-from random import randint
-
 from .string_utils import get_random_string
 
 
 def get_uuid() -> str:
-    return str(get_random_string(randint(32, 64)))
+    return str(get_random_string(16))
